2e4f78be1b6dad82ef4b4090fd02e0cf8470fbff
jhgan00 <jhgan00@gmail.com>
2020-04-28 20:23:07 +0900

    데이터베이스 기능 추가

--- app/src/main/java/org/techtown/malangdiary/DiaryActivity.java
+++ app/src/main/java/org/techtown/malangdiary/DiaryActivity.java
@@ -1,31 +1,49 @@
 package org.techtown.malangdiary;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class DiaryActivity extends AppCompatActivity {
 
     private ImageView imageView;
+
+    Button dateButton;
+    EditText titleEditText;
+    EditText contentEditText;
+    Date today = new Date();
+    SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.KOREA);
+    SQLiteDatabase database;
+    private DatePickerDialog.OnDateSetListener callbackMethod;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
 
+        this.initializeView();
+        this.initializeListener();
+
         imageView = findViewById(R.id.imageUpload);
-
-
-
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,11 +56,7 @@ public class DiaryActivity extends AppCompatActivity {
             //클릭시 앨범에 접근해 사진을 올릴 수 있도록 했음
 
         });
-
-
     }
-
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -73,15 +87,94 @@ public class DiaryActivity extends AppCompatActivity {
     }
 
     public void diary_submit(View v) {
-        Intent diaryIntent = new Intent(this, MainActivity.class);
-        diaryIntent.putExtra("alarm", "일기가 작성되었어요.");
-        setResult(Activity.RESULT_OK, diaryIntent);
+        titleEditText = findViewById(R.id.titleText);
+        contentEditText = findViewById(R.id.contentText);
+        dateButton = findViewById(R.id.dateButton);
 
-        //일기가 입력되어 저장되었으면 OK, 그렇지 않으면 CANCEL이 되어야 함.
+        String date = dateButton.getText().toString();
+        String title = titleEditText.getText().toString();
+        String content = contentEditText.getText().toString();
+
+        // 유효성 검사 통과하면 등록
+        if(validData(date, title, content)) {
+            createDatabase();
+            createTable();
+            insertData(date, title, content);
+
+            Intent diaryIntent = new Intent(this, MainActivity.class);
+            diaryIntent.putExtra("alarm", "일기가 작성되었어요.");
+            setResult(Activity.RESULT_OK, diaryIntent);
+            finish();
+        }
+        else{
+            Toast.makeText(this, "날짜와 제목, 내용을 작성해주세요.", Toast.LENGTH_SHORT).show();
+        }
+        //일기가 입력되어 ㄱ저장되었으면 OK, 그렇지 않으면 CANCEL이 되어야 함.
         //지금 DB에 입력하는 게 구현 안 되었으니 걍 누르면 OK를 출력하도록 했음.
-
-        finish();
     }
 
+    public void onDateButtonClick(View view){
+        try{
+            Date curDate = timeFormat.parse(dateButton.getText().toString());
+            Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Asia/Seoul"));
+            cal.setTime(curDate);
+            int year = cal.get(Calendar.YEAR);
+            int month = cal.get(Calendar.MONTH);
+            int day = cal.get(Calendar.DAY_OF_MONTH);
+            DatePickerDialog dialog = new DatePickerDialog(this, callbackMethod, year, month, day);
+            dialog.show();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
+    public void createDatabase(){
+        database = openOrCreateDatabase("Diary.db", MODE_PRIVATE, null);
+    }
+
+    public void createTable(){
+        String SQL = "CREATE TABLE IF NOT EXISTS DIARIES (DATE TEXT PRIMARY KEY, TITLE TEXT, CONTENT TEXT);";
+        database.execSQL(SQL);
+    }
+
+    public boolean validData(String date, String title, String content){
+        if(date.length()>0 && title.length()>0 && content.length()>0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void insertData(String date, String title, String content){
+        try
+        {
+            if(database != null)
+            {
+                String sql = "INSERT INTO DIARIES VALUES ('" + date + "', '" + title + "', '" + content + "');";
+                database.execSQL(sql);
+            }
+        }
+        catch(Exception e) { e.printStackTrace(); }
+    }
+
+    public void initializeView(){
+        dateButton = findViewById(R.id.dateButton);
+        String todayString = timeFormat.format(today);
+        dateButton.setText(todayString);
+    }
+
+    public void initializeListener(){
+        callbackMethod = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Calendar c = Calendar.getInstance();
+                c.set(year, month, dayOfMonth, 0, 0);
+                String time = timeFormat.format(c.getTime());
+                dateButton = findViewById(R.id.dateButton);
+                dateButton.setText(time);
+            }
+        };
+    }
 }
