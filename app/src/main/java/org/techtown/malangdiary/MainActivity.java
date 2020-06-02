a4f68035764988eb9a8373f83589cc4175a472f1
SC <limsc95@gmail.com>
2020-06-02 14:50:04 +0900

    title bar removed

--- app/src/main/java/org/techtown/malangdiary/MainActivity.java
+++ app/src/main/java/org/techtown/malangdiary/MainActivity.java
@@ -79,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        materialCalendarView.addDecorator();
+        //materialCalendarView.addDecorator();
     }
 
 
